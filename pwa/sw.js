a66c28bb8b451fff7666add135e34a623f829ed0
huyu <huyu@meicai.cn>
2020-01-02 21:32:15 +0800

    test

--- pwa/sw.js
+++ pwa/sw.js
@@ -17,6 +17,25 @@ function needCache(url) {
   return dict[pathname];
 }
 
+function fetchRequest({ request, cache }) {
+  var request2 = request.clone();
+  return fetch(request2).then(function(response) {
+    // Check if we received a valid response
+    if(!response || response.status !== 200 || response.type !== 'basic') {
+      return response;
+    }
+
+    // IMPORTANT:Clone the response. A response is a stream
+    // and because we want the browser to consume the response
+    // as well as the cache consuming the response, we need
+    // to clone it so we have two streams.
+    var responseToCache = response.clone();
+    cache.put(request, responseToCache);
+
+    return response;
+  });
+}
+
 // 添加缓存
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -28,33 +47,16 @@ self.addEventListener('install', function(event) {
 
 // 拦截请求
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-
-      var fetchRequest = event.request.clone();
-      return fetch(fetchRequest).then(function(response) {
-        // Check if we received a valid response
-        if(!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
+  event.respondWith(caches.open(CACHE_NAME).then(function (cache) {
+    return cache.match(request).then(function (response) {
+        if (response && response.ok) {
+            // if (cacheType.type === CACHE_TYPES.PREV_CACHE) {
+            //     fetchRequest({ request, cache });
+            // }
+            return response;
+        } else {
+            return fetchRequest({ request, cache });
         }
-
-        // IMPORTANT:Clone the response. A response is a stream
-        // and because we want the browser to consume the response
-        // as well as the cache consuming the response, we need
-        // to clone it so we have two streams.
-        var responseToCache = response.clone();
-
-        caches.open(CACHE_NAME)
-          .then(function(cache) {
-            cache.put(event.request, responseToCache);
-          });
-
-        return response;
-      });
-    })
-  );
+    });
+  }));
 });
