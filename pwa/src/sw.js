077573c5bd5ffadc8bccafb2135a4cf5998b9a0d
huyu <huyu@meicai.cn>
2020-01-02 19:25:07 +0800

    bugfix

--- pwa/src/sw.js
+++ pwa/src/sw.js
@@ -1,1 +1,49 @@
-console.log('test');
+var CACHE_NAME = 'cachev1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/imgs/q1.png',
+  '/imgs/q_stack.png'
+];
+
+// 添加缓存
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// 拦截请求
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      var fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(function(response) {
+        // Check if we received a valid response
+        if(!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        // IMPORTANT:Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        var responseToCache = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+
+        return response;
+      });
+    })
+  );
+});
--- pwa/src/swIndex.js
+++ pwa/src/swIndex.js
@@ -1,7 +1,21 @@
-debugger;
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('./sw.js').then(function(registration) {
-    debugger;
+    var serviceWorker;
+    if (registration.installing) {
+      serviceWorker = registration.installing;
+      console.log('installing');
+    } else if (registration.waiting) {
+      serviceWorker = registration.waiting;
+      console.log('waiting');
+    } else if (registration.active) {
+      serviceWorker = registration.active;
+      console.log('active');
+    }
+    if (serviceWorker) {
+      serviceWorker.addEventListener('statechange', function (e) {
+
+      });
+    }
   }).catch(function(err) {
     console.error('service worker初始化失败: ', err);
   });
